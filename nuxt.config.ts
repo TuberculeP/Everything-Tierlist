42a0ce29f7d66fe33853c12e683489ac64536999
Félix Laviéville <felix@oplit.fr>
2024-02-26 10:13:08 +0100

    remove ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  extends: ["@nuxt/ui-pro"],
-  modules: ["@nuxt/ui", "@nuxtjs/supabase"],
+  modules: ["@nuxtjs/supabase"],
 });
