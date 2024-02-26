135a4d7e289432c545facd84052c60db77ce57cb
Félix Laviéville <felix@oplit.fr>
2024-02-26 13:20:14 +0100

    log qui marche

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,4 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/supabase"],
-  supabase: {
-    redirectOptions: {
-      login: "/login",
-      callback: "/confirm",
-    },
-  },
 });
--- pages/login.vue
+++ pages/login.vue
@@ -1,22 +1,17 @@
 <script setup lang="ts">
 const supabase = useSupabaseClient();
-const user = useSupabaseUser();
-console.log(user.value);
+const {host, protocol} = useRequestURL();
 const signInWithOAuth = async () => {
   const { error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
+      redirectTo: `${protocol}//${host}/confirm`,
       queryParams: {
         access_type: "offline",
         prompt: "consent",
       },
     },
   });
-  if (error) console.log(error);
-};
-
-const signOut = async () => {
-  const { error } = await supabase.auth.signOut();
   if (error) console.log(error);
 };
 </script>
