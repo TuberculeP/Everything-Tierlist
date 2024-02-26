a17c6dfabbe09300b80d511592755d22d387cf40
Félix Laviéville <felix@oplit.fr>
2024-02-26 10:10:52 +0100

    initial

---
+++ pages/login.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const supabase = useSupabaseClient();
+
+const signInWithOAuth = async () => {
+  const { error } = await supabase.auth.signInWithOAuth({
+    provider: "google",
+    options: {
+      queryParams: {
+        access_type: "offline",
+        prompt: "consent",
+      },
+    },
+  });
+  if (error) console.log(error);
+};
+
+const signOut = async () => {
+  const { error } = await supabase.auth.signOut();
+  if (error) console.log(error);
+};
+</script>
+<template>
+  <button @click="signInWithOAuth">Click</button>
+</template>
